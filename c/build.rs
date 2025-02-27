507badb4af1869e6b8b3a4afdf7e7f15931f1d16
Jacob Bandes-Storch <jacob@foxglove.dev>
2025-02-26 18:57:06 -0800

    simplify, build both static and shared libraries

--- c/build.rs
+++ c/build.rs
@@ -5,7 +5,7 @@ fn main() {
 
     cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings")
-        .write_to_file("generated/include/foxglove-c/foxglove-c.h");
+        .write_to_file("include/foxglove-c/foxglove-c.h");
 
     println!("cargo:rerun-if-changed=cbindgen.toml");
 }
